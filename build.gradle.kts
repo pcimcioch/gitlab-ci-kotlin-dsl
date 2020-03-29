c2fe526acbb81fa6274808e7f08865b85da45491
Przemyslaw Cimcioch <cimcioch.przemyslaw@gmail.com>
2020-03-29 20:05:19 +0200

    Added publishing and signing task

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ repositories {
 plugins {
     kotlin("jvm") version "1.3.70"
     kotlin("plugin.serialization") version "1.3.70"
+    `maven-publish`
+    signing
 }
 
 dependencies {
@@ -35,4 +37,35 @@ tasks {
     }
 }
 
-// TODO add publish task
+java {
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                name.set("GitlabCi Kotlin DSL")
+                description.set("Library providing Kotlin DSL to configure GitlabCI file")
+                url.set("https://github.com/pcimcioch/gitlab-ci-kotlin-dsl")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/pcimcioch/gitlab-ci-kotlin-dsl.git")
+                    developerConnection.set("scm:git:git@github.com:pcimcioch/gitlab-ci-kotlin-dsl.git")
+                    url.set("https://github.com/pcimcioch/gitlab-ci-kotlin-dsl")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["maven"])
+}
